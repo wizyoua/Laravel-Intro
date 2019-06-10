4ca9fdbd4b59cd61d685b9bbc70018dd69de8c91
giannizamora <gianni@rxmg.com>
2019-06-09 19:01:08 -0700

    inital commit

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,7 @@
+@extends('layout')
+
+@section('content')
+<h1>Contact Us</h1>
+<p>Company name</p>
+<p>480-111-1111</p>
+@endsection
---
+++ resources/views/internals/customers.blade.php
@@ -0,0 +1,31 @@
+@extends('layout')
+
+@section('content')
+    <h1>customers</h1>
+    <form action="customers" method="POST" class="pb-5">
+        <p>name:</p>
+        <div class="input-group pb-2">
+            <input type="text" name="name" value="{{ old('name') }}">
+            <div>{{ $errors->first('name') }}</div>
+        </div>
+        <p>email:</p>
+        <div class="input-group pb-2">
+            <input type="text" name="email" value="{{ old('email') }}">
+            <div>{{ $errors->first('email') }}</div>
+        </div>
+
+
+
+
+        <button type="submit">Add Customer</button>
+
+        @csrf
+    </form>
+
+
+    <ul>
+    @foreach ($customers as $customer)
+        <li>{{$customer->name}}  <span class="text-muted">{{$customer->email}}</span></li>
+    @endforeach
+    </ul>
+@endsection
