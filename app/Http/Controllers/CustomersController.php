232df93b9a830b215771c8cd5a976e39a9cbdcda
giannizamora <gianni@rxmg.com>
2019-06-10 12:30:10 -0700

    e12

--- app/Http/Controllers/CustomersController.php
+++ app/Http/Controllers/CustomersController.php
@@ -8,24 +8,27 @@ use Illuminate\Http\Request;
 class CustomersController extends Controller
 {
     public function list(){
+        $activeCustomers = Customer::where('active', 1)->get();
+        $inactiveCustomers = Customer::where('active', 0)->get();
+
         $customers = Customer::all();
 
 
-        return view('internals.customers', [
-            'customers' => $customers
-        ]);
+        return view('internals.customers', compact('activeCustomers', 'inactiveCustomers'));
     }
 
     public function store(){
 
         $data = request()->validate([
             'name' => 'required|min:3',
-            'email' => 'required|email'
+            'email' => 'required|email',
+            'active' => 'required',
         ]);
 
         $customer = new Customer();
         $customer->name = request('name');
         $customer->email = request('email');
+        $customer->active = request('active');
         $customer->save();
 
         return back();
